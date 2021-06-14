7a61172b7b1c62f03a9ddc5f02796efce31761b6
Юра <naerok@icloud.com>
2021-06-14 23:20:12 +0300

    building the application architecture and getting started

---
+++ src/components/CarouselVuetify/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-carousel
+    :continuous="false"
+    :cycle="false"
+    height="400"
+    hide-delimiter-background
+  >
+    <template v-slot:prev="{ on, attrs }">
+      <v-btn color="success" v-bind="attrs" v-on="on">Previous slide</v-btn>
+    </template>
+    <template v-slot:next="{ on, attrs }">
+      <v-btn color="info" v-bind="attrs" v-on="on">Next slide</v-btn>
+    </template>
+    <v-carousel-item v-for="(slide, i) in sliderItems" :key="i">
+      <v-row class="fill-height" align="center" justify="center">
+        <img :src="require('../../assets/images/' + slide)" alt="" />
+      </v-row>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  props: {
+    carouselData: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      sliderItems: ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.v-window-item {
+  width: 800px;
+}
+</style>
---
+++ src/components/carousel/Carousel-item.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="carousel-item">
+    <img :src="require('../../assets/images/' + itemData.image)" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+img {
+  width: 800px;
+  height: 500px;
+}
+</style>
